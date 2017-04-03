c10751257ec97eff9b485e93373c35f8c87bdb68
chris carr and grace stuart <chris+grace@>
2017-04-03 10:47:29 -0700

    update database details for development and testing

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,16 +5,16 @@ public class DatabaseRule extends ExternalResource {
 
   @Override
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/virtual_pets_test", null, null);
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/library_catalog_test", null, null);
   }
 
   @Override
   protected void after() {
     try(Connection con = DB.sql2o.open()) {
-      String deletePersonsQuery = "DELETE FROM persons *;";
-      String deleteMonstersQuery = "DELETE FROM monsters *;";
-      con.createQuery(deletePersonsQuery).executeUpdate();
-      con.createQuery(deleteMonstersQuery).executeUpdate();
+      String deletePatronsQuery = "DELETE FROM patrons *;";
+      String deleteBooksQuery = "DELETE FROM books *;";
+      con.createQuery(deletePatronsQuery).executeUpdate();
+      con.createQuery(deleteBooksQuery).executeUpdate();
     }
   }
 
